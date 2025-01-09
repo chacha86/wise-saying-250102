896dee03ea2e63d68b36f402f3f704ea3b3eae49
chacha86 <taejincha0619@gmail.com>
2025-01-09 10:50:56 +0900

    cont t21 b

--- src/test/java/app/domain/wiseSaying/WiseSayingControllerTest.java
+++ src/test/java/app/domain/wiseSaying/WiseSayingControllerTest.java
@@ -265,7 +265,6 @@ public class WiseSayingControllerTest {
                 """);
 
         assertThat(out)
-                .contains("1 / 작가1 / 명언1")
                 .contains("10 / 작가10 / 명언10");
     }
 
